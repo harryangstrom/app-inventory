91836d658b4c65b0d6466c5a7fe7918e52a6f45b
harryangstrom <harry.angstrom67@gmail.com>
2019-02-10 12:04:33 +0100

    1st commit

---
+++ src/app/product.model.ts
@@ -0,0 +1,16 @@
+/* 
+Product Object
+
+ */
+
+export class Product {
+
+  constructor(
+    public sku: string,
+    public name: string,
+    public imageUrl: string,
+    public department: string[],
+    public price: number) {
+      
+    }
+}
